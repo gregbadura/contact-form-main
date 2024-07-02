9fef10a033db04288ce48bb19612dfd066a98bd1
Grzegorz <grzegorz.bogdan.badura@gmail.com>
2024-07-02 22:19:24 +0200

    '0.0.0.0.2'

---
+++ dist/contact-form-main/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YJAM2HII.mjs";import"./chunk-FSOLM7EK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'contact-form-main';
+
+  @ViewChild('option') option!: ElementRef;
+
+  ngOnInit(): void {
+    const queryoptions = document.getElementsByClassName('option');
+    Array.from(queryoptions).forEach((q: Element) => {
+      q.addEventListener('change', tick);
+    });
+
+    function tick(e: Event) {
+      const state = (e.target as HTMLInputElement).checked;
+      Array.from(queryoptions).forEach((q: Element) => {
+        (q as HTMLInputElement).checked = false;
+      });
+      (e.target as HTMLInputElement).checked = state;
+    }
+  }
 }
